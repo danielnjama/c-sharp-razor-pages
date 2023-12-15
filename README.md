73acab7b4037da7774a5047f1a9265616842e7f7
Daniel Njama Wangari <daniel.njama@prodapt.com>
2023-12-15 12:49:29 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+"# c-sharp-razor-pages" 
---
+++ webapp/Pages/about.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace webapp.Pages
+{
+    public class aboutModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
